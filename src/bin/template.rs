0174486d2b33f7ad91b387350584932454ee0f17
Jeremy Smart <jeremy3141592@gmail.com>
2024-12-01 00:13:02 -0500

    day 1

---
+++ src/bin/template.rs
@@ -0,0 +1,20 @@
+use std::fs;
+
+use anyhow::Result;
+
+fn part1() -> Result<()> {
+    let input = fs::read_to_string("/home/jeremy/github/adventofrust/input/%DAY%")?;
+
+    Ok(())
+}
+
+fn part2() -> Result<()> {
+    let input = fs::read_to_string("/home/jeremy/github/adventofrust/input/%DAY%")?;
+
+    Ok(())
+}
+
+fn main() {
+    part1().unwrap();
+    // part2().unwrap();
+}
