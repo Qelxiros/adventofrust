33eb66a632118f0020395436973c36f26c8adb9b
Jeremy Smart <jeremy3141592@gmail.com>
2024-12-16 00:53:30 -0500

    day 16

---
+++ src/bin/16.rs
@@ -0,0 +1,177 @@
+#![feature(let_chains)]
+
+use std::{
+    cmp::Ordering,
+    collections::{BinaryHeap, HashMap, HashSet, VecDeque},
+};
+
+use adventofrust::utils::grid_idx;
+use itertools::Itertools;
+use regex::Regex;
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Hash)]
+struct State {
+    pos: (usize, usize),
+    cost: usize,
+    dir: (isize, isize),
+}
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // Notice that we flip the ordering on costs.
+        // In case of a tie we compare positions - this step is necessary
+        // to make implementations of `PartialEq` and `Ord` consistent.
+        other
+            .cost
+            .cmp(&self.cost)
+            .then_with(|| self.pos.cmp(&other.pos))
+            .then_with(|| self.dir.cmp(&other.dir))
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+const DIRS: [(isize, isize); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+
+fn part1() {
+    let input = include_str!("/home/jeremy/github/adventofrust/input/16");
+
+    let (grid, Some(idx)) = grid_idx(input, 'S') else {
+        panic!()
+    };
+    let (_, Some(end_idx)) = grid_idx(input, 'E') else {
+        panic!()
+    };
+
+    let mut q = BinaryHeap::new();
+    let mut seen = HashSet::new();
+    q.push(State {
+        pos: idx,
+        cost: 0,
+        dir: (0, 1),
+    });
+    while let Some(State { pos, cost, dir }) = q.pop() {
+        if seen.contains(&(pos, dir)) {
+            continue;
+        }
+        seen.insert((pos, dir));
+        if pos == end_idx {
+            println!("{cost}");
+            return;
+        }
+        for d in DIRS {
+            if d == (-dir.0, -dir.1) {
+                continue;
+            }
+            if let Ok(x) = <isize as std::convert::TryInto<usize>>::try_into(pos.0 as isize + d.0)
+                && let Ok(y) =
+                    <isize as std::convert::TryInto<usize>>::try_into(pos.1 as isize + d.1)
+            {
+                if grid
+                    .get(x)
+                    .and_then(|l| l.get(y))
+                    .is_some_and(|c| *c != '#')
+                {
+                    let inc = if dir == d { 1 } else { 1001 };
+                    let state = State {
+                        pos: (x, y),
+                        cost: cost + inc,
+                        dir: d,
+                    };
+                    q.push(state);
+                }
+            }
+        }
+    }
+}
+
+fn part2() {
+    let input = include_str!("/home/jeremy/github/adventofrust/input/16");
+
+    let (grid, Some(idx)) = grid_idx(input, 'S') else {
+        panic!()
+    };
+    let (_, Some(end_idx)) = grid_idx(input, 'E') else {
+        panic!()
+    };
+
+    let mut q = BinaryHeap::new();
+    let mut pred = HashMap::<State, Vec<State>>::new();
+    let mut seen = HashSet::new();
+    let mut tiles = HashSet::new();
+    let mut best_cost = None;
+    q.push(State {
+        pos: idx,
+        cost: 0,
+        dir: (0, 1),
+    });
+    tiles.insert(idx);
+    tiles.insert(end_idx);
+    while let Some(State { pos, cost, dir }) = q.pop() {
+        if seen.contains(&(pos, dir)) {
+            continue;
+        }
+        seen.insert((pos, dir));
+        if pos == end_idx && best_cost.is_none_or(|c: usize| c == cost) {
+            best_cost = Some(cost);
+            let mut states = VecDeque::new();
+            states.push_back(State { pos, cost, dir });
+            while let Some(s) = states.pop_front() {
+                if let Some(s) = pred.get(&s) {
+                    tiles.extend(s.iter().map(|a| a.pos));
+                    states.extend(s);
+                }
+            }
+        }
+        for d in DIRS {
+            if d == (-dir.0, -dir.1) {
+                continue;
+            }
+            if let Ok(x) = <isize as std::convert::TryInto<usize>>::try_into(pos.0 as isize + d.0)
+                && let Ok(y) =
+                    <isize as std::convert::TryInto<usize>>::try_into(pos.1 as isize + d.1)
+            {
+                if grid
+                    .get(x)
+                    .and_then(|l| l.get(y))
+                    .is_some_and(|c| *c != '#')
+                {
+                    let inc = if dir == d { 1 } else { 1001 };
+                    let state = State {
+                        pos: (x, y),
+                        cost: cost + inc,
+                        dir: d,
+                    };
+                    q.push(state);
+                    if let Some(v) = pred.get_mut(&state) {
+                        v.push(State { pos, cost, dir });
+                    } else {
+                        pred.insert(state, vec![State { pos, cost, dir }]);
+                    }
+                }
+            }
+        }
+    }
+
+    for (idx, row) in grid.into_iter().enumerate() {
+        let mut s = String::new();
+        for (i, j) in row.into_iter().enumerate() {
+            if j == '.' && tiles.contains(&(idx, i)) {
+                s.push('O');
+            } else {
+                s.push(j)
+            }
+        }
+        println!("{s}");
+    }
+
+    println!("{}", tiles.len());
+}
+
+fn main() {
+    part1();
+    part2();
+}
--- src/utils.rs
+++ src/utils.rs
@@ -1,36 +1,85 @@
-use std::{
-    fmt::{Debug, Display},
-    str::FromStr,
-};
-
 use itertools::Itertools;
-use num::{Signed, Unsigned};
+use num::{Integer, Signed, Unsigned};
 use regex::Regex;
+use std::{fmt::Debug, str::FromStr};
 
-pub fn grid(input: String) -> Vec<Vec<char>> {
+pub fn grid(input: &str) -> Vec<Vec<char>> {
     input.lines().map(|s| s.chars().collect_vec()).collect_vec()
 }
 
-pub fn integers_signed<T>(input: String) -> Vec<T>
+pub fn grid_idx(input: &str, search: char) -> (Vec<Vec<char>>, Option<(usize, usize)>) {
+    let mut loc = None;
+    (
+        input
+            .lines()
+            .enumerate()
+            .map(|(idx, s)| {
+                s.chars()
+                    .enumerate()
+                    .map(|(i, c)| {
+                        if c == search {
+                            loc = Some((idx, i))
+                        }
+                        c
+                    })
+                    .collect_vec()
+            })
+            .collect_vec(),
+        loc,
+    )
+}
+
+pub fn grid_with<T>(input: &str, f: impl Fn(char) -> T) -> Vec<Vec<T>> {
+    input
+        .lines()
+        .map(|s| s.chars().map(&f).collect_vec())
+        .collect_vec()
+}
+
+pub fn grid_idx_with<T>(
+    input: &str,
+    search: char,
+    f: impl Fn(char) -> T,
+) -> (Vec<Vec<T>>, Option<(usize, usize)>) {
+    let mut loc = None;
+    (
+        input
+            .lines()
+            .enumerate()
+            .map(|(idx, s)| {
+                s.chars()
+                    .enumerate()
+                    .map(|(i, c)| {
+                        if c == search {
+                            loc = Some((idx, i))
+                        }
+                        f(c)
+                    })
+                    .collect_vec()
+            })
+            .collect_vec(),
+        loc,
+    )
+}
+
+pub fn integers_signed<T>(input: &str) -> Vec<T>
 where
-    T: Signed,
-    T: FromStr,
+    T: Signed + Integer + FromStr,
     <T as FromStr>::Err: Debug,
 {
     let re = Regex::new(r"-?\d+").unwrap();
-    re.find_iter(&input)
+    re.find_iter(input)
         .map(|m| m.as_str().parse::<T>().unwrap())
         .collect_vec()
 }
 
-pub fn integers_unsigned<T>(input: String) -> Vec<T>
+pub fn integers_unsigned<T>(input: &str) -> Vec<T>
 where
-    T: Unsigned,
-    T: FromStr,
+    T: Unsigned + Integer + FromStr,
     <T as FromStr>::Err: Debug,
 {
     let re = Regex::new(r"\d+").unwrap();
-    re.find_iter(&input)
+    re.find_iter(input)
         .map(|m| m.as_str().parse::<T>().unwrap())
         .collect_vec()
 }
